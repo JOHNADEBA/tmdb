15f534788623a179039a5cb26bbf582e241aeac8
john.adeba <John.Adeba@endava.com>
2022-11-30 14:03:07 +0100

    first deploy

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="app">
-    <div class="menu">
-      <SideBar
-        :allGenres="allGenres"
-        @name="getSingleGenre"
-        @search="callGetGenreMovies()"
-      />
-    </div>
-    <div class="movies">
-      <Movies :allMovies="allMovies" :loading="loading" :errors="errors" />
-      <div v-if="showLoadMore" class="load-btn">
-        <button @click="loadMore()">Load More</button>
-      </div>
-    </div>
-  </div>
+	<div class="app">
+		<div class="menu">
+			<SideBar
+				:allGenres="allGenres"
+				@name="getSingleGenre"
+				@search="callGetGenreMovies()"
+			/>
+		</div>
+		<div class="movies">
+			<Movies :allMovies="allMovies" :loading="loading" :errors="errors" />
+			<div v-if="showLoadMore" class="load-btn">
+				<button @click="loadMore()">Load More</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -23,90 +23,90 @@ import Movies from "./components/Movies.vue";
 import axios from "axios";
 
 export default {
-  name: "App",
-  components: {
-    SideBar,
-    Movies,
-  },
-  data() {
-    return {
-      baseUrl: "https://api.themoviedb.org/3",
-      imgBaseUrl: "https://image.tmdb.org/t/p/w500",
-      apiKey: process.env.VUE_APP_API_KEY,
-      lang: "language=en-US",
-      page: 1,
-      allMovies: [],
-      allGenres: [],
-      selectedGenre: [],
-      loading: true,
-      errors: "",
-      showLoadMore: true,
-    };
-  },
+	name: "App",
+	components: {
+		SideBar,
+		Movies,
+	},
+	data() {
+		return {
+			baseUrl: "https://api.themoviedb.org/3",
+			imgBaseUrl: "https://image.tmdb.org/t/p/w500",
+			apiKey: process.env.VUE_APP_API_KEY,
+			lang: "language=en-US",
+			page: 1,
+			allMovies: [],
+			allGenres: [],
+			selectedGenre: [],
+			loading: true,
+			errors: "",
+			showLoadMore: true,
+		};
+	},
 
-  mounted() {
-    this.fetchAllMovies(this.page);
-    this.fetchAllGenres();
-  },
-  methods: {
-    async fetchAllMovies(page) {
-      this.selectedGenre = [];
-      const data = await axios.get(
-        `${this.baseUrl}/discover/movie?sort_by=popularity.desc&${this.apiKey}&${this.lang}&page=${page}`
-      );
+	mounted() {
+		this.fetchAllMovies(this.page);
+		this.fetchAllGenres();
+	},
+	methods: {
+		async fetchAllMovies(page) {
+			this.selectedGenre = [];
+			const data = await axios.get(
+				`${this.baseUrl}/discover/movie?sort_by=popularity.desc&${this.apiKey}&${this.lang}&page=${page}`
+			);
 
-      await data.data.results.forEach((ele) => this.allMovies.push(ele));
+			await data.data.results.forEach((ele) => this.allMovies.push(ele));
 
-      this.loading = false;
-    },
-    loadMore() {
-      this.page += 1;
-      this.selectedGenre.length > 0
-        ? this.getGenreMovies(this.page)
-        : this.fetchAllMovies(this.page);
-    },
+			this.loading = false;
+		},
+		loadMore() {
+			this.page += 1;
+			this.selectedGenre.length > 0
+				? this.getGenreMovies(this.page)
+				: this.fetchAllMovies(this.page);
+		},
 
-    async fetchAllGenres() {
-      const data = await axios.get(
-        `${this.baseUrl}/genre/movie/list?${this.apiKey}&${this.lang}`
-      );
-      this.showLoadMore = true;
-      this.errors = "";
-      this.allGenres = await data.data.genres;
-      this.loading = false;
-    },
-    getSingleGenre(genre) {
-      this.page = 1;
+		async fetchAllGenres() {
+			const data = await axios.get(
+				`${this.baseUrl}/genre/movie/list?${this.apiKey}&${this.lang}`
+			);
+			this.showLoadMore = true;
+			this.errors = "";
+			this.allGenres = await data.data.genres;
+			this.loading = false;
+		},
+		getSingleGenre(genre) {
+			this.page = 1;
 
-      if (!this.selectedGenre.includes(genre)) {
-        this.selectedGenre.push(genre);
-      } else {
-        this.selectedGenre.splice(this.selectedGenre.indexOf(genre), 1);
-      }
-    },
-    async getGenreMovies(page) {
-      this.loading = true;
+			if (!this.selectedGenre.includes(genre)) {
+				this.selectedGenre.push(genre);
+			} else {
+				this.selectedGenre.splice(this.selectedGenre.indexOf(genre), 1);
+			}
+		},
+		async getGenreMovies(page) {
+			this.loading = true;
 
-      const data = await axios.get(
-        `${this.baseUrl}/discover/movie?${this.apiKey}&${this.lang}&with_genres=${this.selectedGenre}&page=${page}`
-      );
-      if (data.data.results.length > 0) {
-        await data.data.results.forEach((ele) => this.allMovies.push(ele));
-        this.showLoadMore = true;
-        this.errors = "";
-      } else {
-        this.errors = "No items were found that match your query.";
-        this.showLoadMore = false;
-      }
-      this.loading = false;
-    },
+			const data = await axios.get(
+				`${this.baseUrl}/discover/movie?${this.apiKey}&${this.lang}&with_genres=${this.selectedGenre}&page=${page}`
+			);
+			if (data.data.results.length > 0) {
+				await data.data.results.forEach((ele) => this.allMovies.push(ele));
+				this.showLoadMore = true;
+				this.errors = "";
+			} else {
+				this.errors = "No items were found that match your query.";
+				this.showLoadMore = false;
+			}
+			this.loading = false;
+		},
 
-    callGetGenreMovies() {
-      this.allMovies = [];
-      this.page = 1;
-      this.getGenreMovies(this.page);
-    },
-  },
+		callGetGenreMovies() {
+			this.allMovies = [];
+			this.page = 1;
+			this.getGenreMovies(this.page);
+		},
+	},
 };
 </script>
 
@@ -115,49 +115,76 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@700&display=swap");
 
 * {
-  padding: 0;
-  margin: 0;
-  box-sizing: 0;
+	padding: 0;
+	margin: 0;
+	box-sizing: 0;
 }
 body {
-  width: 100%;
-  color: #2c3e50;
+	width: 100%;
+	color: #2c3e50;
 }
 .app {
-  @include bigFlexCenter;
-  width: 95%;
-  margin: 3rem auto;
-  font-family: Source Sans Pro, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+	@include bigFlexCenter;
+	width: 95%;
+	margin: 3rem auto;
+	font-family: Source Sans Pro, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
 }
 h2 {
-  margin-bottom: 2rem;
+	margin-bottom: 2rem;
 }
 .menu {
-  width: 18rem;
+	width: 18rem;
+	margin-right: 1rem;
 }
 .movies {
-  width: calc(100% - 20rem);
+	width: calc(100% - 20rem);
 }
 .load-btn {
-  margin: 1.2rem 0 0 1rem;
-  background: $bg-btn-color;
-  border-radius: $border-radius;
-  padding: 0.8rem 0.9rem;
-  text-align: center;
+	margin: 1.2rem 0 0 1rem;
+	background: $bg-btn-color;
+	border-radius: $border-radius;
+	padding: 0.8rem 0.9rem;
+	text-align: center;
 
-  & button {
-    cursor: pointer;
-    background: none;
-    border: none;
-    font-size: 1.4rem;
-    color: white;
-    font-weight: bold;
+	& button {
+		cursor: pointer;
+		background: none;
+		border: none;
+		font-size: 1.4rem;
+		color: white;
+		font-weight: bold;
 
-    &:hover {
-      color: black;
-    }
-  }
+		&:hover {
+			color: black;
+		}
+	}
+}
+
+@media screen and (max-width: 995px) {
+	.app {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+	}
+	.menu {
+		text-align: center;
+		margin: auto;
+		margin-bottom: 1rem;
+	}
+	.movies {
+		width: 90%;
+		margin: auto;
+	}
+}
+
+@media screen and (max-width: 300px) {
+	.menu {
+		width: 90%;
+	}
+	.movies {
+		width: 100%;
+	}
 }
 </style>
--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,60 +1,60 @@
 <template>
-  <div class="movies-container">
-    <p class="errors" v-if="errors">{{ errors }}</p>
-    <div class="spinner" v-if="loading">
-      <i class="fas fa-spinner fa-pulse"></i>
-    </div>
-    <div
-      v-else
-      class="movie-container"
-      v-for="(movie, index) in allMovies"
-      :key="index"
-    >
-      <img
-        class="poster"
-        :src="imgBaseUrl + movie.poster_path"
-        alt="movie.original_title"
-      />
-      <h5>{{ movie.title }}</h5>
-      <p class="date">{{ convertDate(movie.release_date) }}</p>
-      <div class="vote">
-        <div class="vote-color">
-          <ve-progress
-            :progress="movie.vote_average * 10"
-            :data="circles"
-            :angle="-90"
-            :color="convertColor(movie.vote_average)"
-            :empty-color="convertBackgroundColor(movie.vote_average)"
-            empty-color-fill="black"
-            :size="50"
-            :thickness="3"
-            empty-thickness="5%"
-            line-mode="normal"
-            line-position="in "
-            empty-line-position="in"
-            :hide-legend="false"
-            :legend="movie.vote_average * 10"
-            :legend-formatter="
-              ({ currentValue }) =>
-                `${new Intl.NumberFormat('en-US').format(currentValue)} `
-            "
-            legend-class="legend-custom-style"
-            dash="60 0.9"
-            animation="700 400"
-            :noData="false"
-            :loading="false"
-            :loader="convertColor(movie.vote_average)"
-            font-color="white"
-            :half="false"
-            :gap="10"
-            font-size="0.9rem"
-          >
-          </ve-progress>
-          <p class="percent">%</p>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="movies-container">
+		<p class="errors" v-if="errors">{{ errors }}</p>
+		<div class="spinner" v-if="loading">
+			<i class="fas fa-spinner fa-pulse"></i>
+		</div>
+		<div
+			v-else
+			class="movie-container"
+			v-for="(movie, index) in allMovies"
+			:key="index"
+		>
+			<img
+				class="poster"
+				:src="imgBaseUrl + movie.poster_path"
+				alt="movie.original_title"
+			/>
+			<h5>{{ movie.title }}</h5>
+			<p class="date">{{ convertDate(movie.release_date) }}</p>
+			<div class="vote">
+				<div class="vote-color">
+					<ve-progress
+						:progress="movie.vote_average * 10"
+						:data="circles"
+						:angle="-90"
+						:color="convertColor(movie.vote_average)"
+						:empty-color="convertBackgroundColor(movie.vote_average)"
+						empty-color-fill="black"
+						:size="50"
+						:thickness="3"
+						empty-thickness="5%"
+						line-mode="normal"
+						line-position="in "
+						empty-line-position="in"
+						:hide-legend="false"
+						:legend="movie.vote_average * 10"
+						:legend-formatter="
+							({ currentValue }) =>
+								`${new Intl.NumberFormat('en-US').format(currentValue)} `
+						"
+						legend-class="legend-custom-style"
+						dash="60 0.9"
+						animation="700 400"
+						:noData="false"
+						:loading="false"
+						:loader="convertColor(movie.vote_average)"
+						font-color="white"
+						:half="false"
+						:gap="10"
+						font-size="0.9rem"
+					>
+					</ve-progress>
+					<p class="percent">%</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -62,133 +62,141 @@ import moment from "moment";
 import { VeProgress } from "vue-ellipse-progress";
 
 export default {
-  components: {
-    VeProgress,
-  },
-  name: "Movies",
-  emits: ["load"],
-  props: ["allMovies", "loading", "errors"],
-  data() {
-    return {
-      imgBaseUrl: "https://image.tmdb.org/t/p/w500",
-    };
-  },
+	components: {
+		VeProgress,
+	},
+	name: "Movies",
+	emits: ["load"],
+	props: ["allMovies", "loading", "errors"],
+	data() {
+		return {
+			imgBaseUrl: "https://image.tmdb.org/t/p/w500",
+		};
+	},
 
-  methods: {
-    convertDate(dateString) {
-      return moment(dateString).format("DD MMM YYYY");
-    },
-    convertColor(x) {
-      if (x >= 7) return "rgb(6, 214, 6, 0.7)";
-      else if (x >= 4 && x < 7) return "rgb(247, 247, 31, 0.7)";
-      else if (x >= 0 && x < 4) return "rgb(126, 125, 125, 0.7)";
-    },
-    convertBackgroundColor(x) {
-      if (x >= 7) return "rgb(6, 214, 6, 0.3)";
-      else if (x >= 4 && x < 7) return "rgb(247, 247, 31, 0.3)";
-      else if (x >= 0 && x < 4) return "rgb(126, 125, 125, 0.3)";
-    },
-  },
+	methods: {
+		convertDate(dateString) {
+			return moment(dateString).format("DD MMM YYYY");
+		},
+		convertColor(x) {
+			if (x >= 7) return "rgb(6, 214, 6, 0.7)";
+			else if (x >= 4 && x < 7) return "rgb(247, 247, 31, 0.7)";
+			else if (x >= 0 && x < 4) return "rgb(126, 125, 125, 0.7)";
+		},
+		convertBackgroundColor(x) {
+			if (x >= 7) return "rgb(6, 214, 6, 0.3)";
+			else if (x >= 4 && x < 7) return "rgb(247, 247, 31, 0.3)";
+			else if (x >= 0 && x < 4) return "rgb(126, 125, 125, 0.3)";
+		},
+	},
 };
 </script>
 
-<style lang='scss'>
+<style lang="scss">
 @import "@/assets/_shared.scss";
 
 .spinner {
-  font-weight: bolder;
-  font-size: 5rem;
-  margin: 15rem auto;
+	font-weight: bolder;
+	font-size: 5rem;
+	margin: 15rem auto;
 }
 
 .errors {
-  margin: 1rem;
+	margin: 1rem;
 }
 
 .movies-container {
-  @include bigFlexBetweenAlign;
-  margin: 3rem 0 0 1rem;
-  flex-wrap: wrap;
+	@include bigFlexBetweenAlign;
+	// margin: 3rem 0 0 1rem;
+	flex-wrap: wrap;
 }
 .movie-container {
-  width: 19%;
-  height: 25rem;
-  border: $side-border;
-  border-radius: $border-radius;
-  box-shadow: $shadow;
-  margin: 0.5rem 0.1rem;
-  padding-bottom: 1rem;
-  position: relative;
-  cursor: pointer;
+	width: 19%;
+	height: 25rem;
+	border: $side-border;
+	border-radius: $border-radius;
+	box-shadow: $shadow;
+	margin: 0.5rem 0.1rem;
+	padding-bottom: 1rem;
+	position: relative;
+	cursor: pointer;
 
-  & .poster {
-    width: 100%;
-    height: 70%;
-    border-top-left-radius: $border-radius;
-    border-top-right-radius: $border-radius;
-  }
+	& .poster {
+		width: 100%;
+		height: 70%;
+		border-top-left-radius: $border-radius;
+		border-top-right-radius: $border-radius;
+	}
 
-  & h5 {
-    margin: 1.3rem 0.5rem 0;
-    padding-top: 0.7rem;
-    font-weight: bolder;
-    font-size: 1rem;
-  }
+	& h5 {
+		margin: 1.3rem 0.5rem 0;
+		padding-top: 0.7rem;
+		font-weight: bolder;
+		font-size: 1rem;
+	}
 
-  & .date {
-    margin: 0.3rem 0.2rem;
-    font-size: 1rem;
-    padding: 0 0.5rem;
-    opacity: 0.6;
-  }
+	& .date {
+		margin: 0.3rem 0.2rem;
+		font-size: 1rem;
+		padding: 0 0.5rem;
+		opacity: 0.6;
+	}
 
-  & .vote {
-    @include bigFlexCenter;
-    position: absolute;
-    bottom: 6.5rem;
-    left: 1rem;
-    align-items: center;
+	& .vote {
+		@include bigFlexCenter;
+		position: absolute;
+		bottom: 6.5rem;
+		left: 1rem;
+		align-items: center;
 
-    & .vote-color {
-      position: relative;
+		& .vote-color {
+			position: relative;
 
-      & .percent {
-        position: absolute;
-        top: 1rem;
-        left: 2rem;
-        font-size: 0.5rem;
-        color: $bg-white;
-      }
-    }
-  }
+			& .percent {
+				position: absolute;
+				top: 1rem;
+				left: 2rem;
+				font-size: 0.5rem;
+				color: $bg-white;
+			}
+		}
+	}
 }
 
 @media screen and (min-width: 1330px) {
-  .movie-container {
-    width: 18%;
-    margin: 0.5rem 0.5rem;
-  }
+	.movie-container {
+		width: 18%;
+		margin: 0.5rem 0.5rem;
+	}
 }
 
 @media screen and (max-width: 1327px) {
-  .movie-container {
-    width: 22%;
-  }
+	.movie-container {
+		width: 22%;
+	}
 }
 @media screen and (max-width: 1153px) {
-  .movie-container {
-    width: 30%;
-  }
+	.movie-container {
+		width: 30%;
+	}
 }
-@media screen and (max-width: 955px) {
-  .movie-container {
-    width: 45%;
-  }
+
+@media screen and (max-width: 720px) {
+	.movie-container {
+		width: 45%;
+	}
 }
-@media screen and (max-width: 755px) {
-  .movie-container {
-    width: 70%;
-    margin-left: 3rem;
-  }
+@media screen and (max-width: 485px) {
+	.movie-container {
+		margin: 0.5rem auto;
+		width: 90%;
+	}
+}
+
+@media screen and (max-width: 300px) {
+	.movie-container {
+		margin: 0.5rem 0;
+		width: 100%;
+	}
 }
 </style>
